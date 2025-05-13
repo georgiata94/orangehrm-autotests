9e0bcf58b2bfd537fc22b880c16416b3cde6ee4b
G. Atanasov <georgi.ata49@gmail.com>
2025-05-13 22:26:13 +0300

    Enhance BaseTest class for improved stability and flexibility.

--- src/test/java/com/demoproject/tests/BaseTest.java
+++ src/test/java/com/demoproject/tests/BaseTest.java
@@ -3,14 +3,22 @@ package com.demoproject.tests;
 import com.demoproject.utils.ActionHelper;
 import com.demoproject.utils.ConfigReader;
 import com.demoproject.utils.MyWebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 
 public abstract class BaseTest {
@@ -35,9 +43,12 @@ public abstract class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
         synchronized (lock) {
             try {
+                if (ITestResult.FAILURE == result.getStatus()) {
+                    captureScreenshot(result.getMethod().getMethodName());
+                }
                 logger.info("Cleaning up test...");
                 if (driver != null) {
                     logger.debug("Deleting cookies...");
@@ -49,6 +60,20 @@ public abstract class BaseTest {
                 driver = null;
                 logger.info("Cleanup completed");
             }
+        }
+    }
+
+    public void captureScreenshot(String methodName) {
+        try {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File source = ts.getScreenshotAs(OutputType.FILE);
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String screenshotName = "screenshot_" + methodName + "_" + timestamp + ".png";
+            File destination = new File("screenshots/" + screenshotName);
+            FileUtils.copyFile(source, destination);
+            logger.info("Screenshot taken: {}", screenshotName);
+        } catch (IOException e) {
+            logger.error("Failed to capture screenshot: ", e);
         }
     }
 }
